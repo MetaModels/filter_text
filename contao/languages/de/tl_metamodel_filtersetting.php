12d981ec7ab325a21cd40e630e927c411488f1bc
Mini Model <minimodel@metamodel.me>
2016-09-02 12:15:27 +0200

    Updated translations from transifex.

--- contao/languages/de/tl_metamodel_filtersetting.php
+++ contao/languages/de/tl_metamodel_filtersetting.php
@@ -8,17 +8,19 @@
  *
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  *
- * last-updated: 2013-05-13T22:14:41+02:00
+ * last-updated: 2016-09-02T04:13:02+02:00
  */
 
-
-$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['references']['all']        = 'suche alle Wörter (UND)';
-$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['references']['any']        = 'suche einige Wörter (ODER)';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['delimiter']['0']           = 'Trennzeichen (Delimeter)';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['delimiter']['1']           = 'Geben Sie das Trennzeichen für den Suchtyp "einige" oder "alle" ein - der Standardtrenner ist ein Leerzeichen.';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['placeholder']['0']         = 'Platzhalter';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['placeholder']['1']         = 'Zeigen Sie diesen Text, solange das Feld leer ist (erfordert HTML5). ';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['references']['all']        = 'suche einige Wörter (ODER)';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['references']['any']        = 'suche alle Wörter (UND)';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['references']['beginswith'] = 'beginnt mit Suchbegriff';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['references']['endswith']   = 'endet mit Suchbegriff';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['references']['exact']      = 'exakte Suche';
-$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['delimiter']['0']           = 'Trennzeichen (Delimeter)';
-$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['delimiter']['1']           = 'Geben Sie das Trennzeichen für den Suchtyp "einige" oder "alle" ein - der Standardtrenner ist ein Leerzeichen.';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['textsearch']['0']          = 'Suchtyp';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['textsearch']['1']          = 'Findet Textstellen.';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['typenames']['text']        = 'Textfilter';
+
