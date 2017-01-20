fa348e3ee9ac638d12243bd86119498425947e10
Ingolf Steinhardt <info@e-spin.de>
2017-01-20 22:07:06 +0100

    Hotfix Finalize 2.0.0

--- src/MetaModels/Filter/Setting/TextFilterSettingTypeFactory.php
+++ src/MetaModels/Filter/Setting/TextFilterSettingTypeFactory.php
@@ -1,8 +1,9 @@
 <?php
+
 /**
  * This file is part of MetaModels/filter_text.
  *
- * (c) 2012-2016 The MetaModels team.
+ * (c) 2012-2017 The MetaModels team.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -14,7 +15,8 @@
  * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
  * @author     Christopher Boelter <christopher@boelter.eu>
  * @author     Marc Reimann <reimann@mediendepot-ruhr.de>
- * @copyright  2012-2016 The MetaModels team.
+ * @author     Ingolf Steinhardt <info@e-spin.de>
+ * @copyright  2012-2017 The MetaModels team.
  * @license    https://github.com/MetaModels/filter_text/blob/master/LICENSE LGPL-3.0
  * @filesource
  */
