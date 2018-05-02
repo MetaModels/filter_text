05581ec54581ddbd47eb33e77989d1693375ab34
Mini Model <minimodel@metamodel.me>
2018-05-02 21:14:42 +0200

    Updated translations from transifex.

--- contao/languages/de/tl_metamodel_filtersetting.php
+++ contao/languages/de/tl_metamodel_filtersetting.php
@@ -8,7 +8,7 @@
  *
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  *
- * last-updated: 2018-03-06T09:59:33+01:00
+ * last-updated: 2018-05-02T21:14:42+02:00
  */
 
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['delimiter']['0']           = 'Trennzeichen (Delimeter)';
@@ -17,11 +17,11 @@ $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['pattern']['0']             = 
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['pattern']['1']             = 'Erstelle ein <a href="https://dev.mysql.com/doc/refman/5.6/en/regexp.html" onclick="window.open(this.href)">RegExp-Muster</a> mit einem Platzhalter "%s" für die Suche; mehr dazu bei MySQL regexp. ';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['placeholder']['0']         = 'Platzhalter';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['placeholder']['1']         = 'Zeigen Sie diesen Platzhaltertext an, solange das Feld leer ist (erfordert HTML5). ';
-$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['references']['all']        = 'suche einige Wörter (ODER)';
-$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['references']['any']        = 'suche alle Wörter (UND)';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['references']['all']        = 'suche alle Wörter (UND)';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['references']['any']        = 'suche einige Wörter (ODER)';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['references']['beginswith'] = 'beginnt mit Suchbegriff';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['references']['endswith']   = 'endet mit Suchbegriff';
-$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['references']['exact']      = 'Genaue Suche';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['references']['exact']      = 'genaue Suche';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['references']['regexp']     = 'eigener regulärer Ausdruck (RegExp)';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['textsearch']['0']          = 'Suchtyp';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['textsearch']['1']          = 'Dieser Filter findet Textstellen.';
