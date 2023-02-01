2bf1133e7c46e669f4e13499f92aeea28c262797
Mini Model <minimodel@metamodel.me>
2023-02-01 17:11:13 +0100

    Updated translations from transifex.

--- src/Resources/contao/languages/de/tl_metamodel_filtersetting.php
+++ src/Resources/contao/languages/de/tl_metamodel_filtersetting.php
@@ -1,22 +1,30 @@
 <?php
 /**
- * Translations are managed using Transifex. To create a new translation
+ * This file is part of MetaModels/filter_text.
+ *
+ * (c) 2012-2023 The MetaModels team.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * Translations are managed automatically using Transifex. To create a new translation
  * or to help to maintain an existing one, please register at transifex.com.
  *
- * @link http://help.transifex.com/intro/translating.html
- * @link https://www.transifex.com/projects/p/metamodels/language/de/
+ * Last-updated: 2023-02-01T16:11:13+00:00
  *
- * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
- *
- * last-updated: 2018-05-02T21:14:42+02:00
+ * @copyright 2012-2023 The MetaModels team.
+ * @license   https://github.com/MetaModels/filter_text/blob/master/LICENSE LGPL-3.0-or-later
+ * @link      https://www.transifex.com/metamodels/public/
+ * @link      https://www.transifex.com/signup/?join_project=metamodels
  */
+
 
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['delimiter']['0']           = 'Trennzeichen (Delimeter)';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['delimiter']['1']           = 'Geben Sie das Trennzeichen für den Suchtyp "einige" oder "alle" an. Das Standardtrennzeichen ist das Leerzeichen.';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['pattern']['0']             = 'RegExp-Muster';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['pattern']['1']             = 'Erstelle ein <a href="https://dev.mysql.com/doc/refman/5.6/en/regexp.html" onclick="window.open(this.href)">RegExp-Muster</a> mit einem Platzhalter "%s" für die Suche; mehr dazu bei MySQL regexp. ';
-$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['placeholder']['0']         = 'Platzhalter';
-$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['placeholder']['1']         = 'Zeigen Sie diesen Platzhaltertext an, solange das Feld leer ist (erfordert HTML5). ';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['references']['all']        = 'suche alle Wörter (UND)';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['references']['any']        = 'suche einige Wörter (ODER)';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['references']['beginswith'] = 'beginnt mit Suchbegriff';
--- src/Resources/contao/languages/ru/tl_metamodel_filtersetting.php
+++ src/Resources/contao/languages/ru/tl_metamodel_filtersetting.php
@@ -2,7 +2,7 @@
 /**
  * This file is part of MetaModels/filter_text.
  *
- * (c) 2012-2019 The MetaModels team.
+ * (c) 2012-2023 The MetaModels team.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -12,9 +12,9 @@
  * Translations are managed automatically using Transifex. To create a new translation
  * or to help to maintain an existing one, please register at transifex.com.
  *
- * Last-updated: 2019-01-21T20:43:40+01:00
+ * Last-updated: 2023-02-01T16:11:13+00:00
  *
- * @copyright 2012-2019 The MetaModels team.
+ * @copyright 2012-2023 The MetaModels team.
  * @license   https://github.com/MetaModels/filter_text/blob/master/LICENSE LGPL-3.0-or-later
  * @link      https://www.transifex.com/metamodels/public/
  * @link      https://www.transifex.com/signup/?join_project=metamodels
@@ -25,8 +25,6 @@ $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['delimiter']['0']           = 
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['delimiter']['1']           = 'Установите разделитель для типа поиска «любой» или «все», разделитель по умолчанию - это пробел.';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['pattern']['0']             = 'Шаблон регулярного выражения';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['pattern']['1']             = 'Задайте <a href="https://dev.mysql.com/doc/refman/5.6/en/regexp.html" onclick="window.open(this.href)">шаблон регулярного выражения</a> только с одним заполнителем "%s" для вашей строки поиска. Дополнительную информацию см. в MySQL регулярные выражения.';
-$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['placeholder']['0']         = 'Заполнитель';
-$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['placeholder']['1']         = 'Показывать этот текст, если оно пустое (требуется HTML5).';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['references']['all']        = 'поиск всех слов (И)';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['references']['any']        = 'поиск по любому слову (ИЛИ)';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['references']['beginswith'] = 'начинается критерием поиска';
